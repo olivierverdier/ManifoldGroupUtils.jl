9771e6004ddc6089767381ff6db08a8d09f86ab1
Olivier Verdier <olivier.verdier@gmail.com>
2024-06-20 14:05:15 +0200

    update dependency on Manifold.jl

--- src/GroupTools.jl
+++ src/GroupTools.jl
@@ -2,19 +2,23 @@ module GroupTools
 
 import ManifoldsBase
 import Manifolds:
-    TangentSpace,
+    AbstractDecoratorManifold, TangentSpace,
     manifold_dimension,
     AbstractBasis, get_vector_lie, get_coordinates,
     identity_element
 import Manifolds:
+    Identity
+import Manifolds:
     AbstractGroupAction, base_group, group_manifold,
     LeftAction, RightAction, LeftSide, RightSide, apply, apply_diff_group,
-    translate_diff, inv_diff, switch_side
+    translate_diff, inv_diff, switch_side,
+    adjoint_action
 import LinearAlgebra # just for `I`, the identity object
 
 include("Matrix.jl")
 include("TestUtils.jl")
 
 algebra(G) = TangentSpace(G, identity_element(G))
+inverse_adjoint_action(G::AbstractDecoratorManifold, p, X) = adjoint_action(G, inv(G, p), X)
 
 end
