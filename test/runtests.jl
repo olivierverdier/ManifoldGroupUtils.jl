9091c005958cc4d88b090de54c2caf067b4bd2dc
Olivier Verdier <olivier.verdier@gmail.com>
2024-06-29 22:41:32 +0200

    Remove weak test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,14 +6,6 @@ import ManifoldsBase
 import Random
 rng = Random.default_rng()
 
-@testset "GroupTools.jl" begin
-    G = Orthogonal(3)
-    op(ξ) = ξ
-    # mat = [1. 0 0 ; 0 1 0; 0 0 1]
-    mat = [1. 0  ; 0 1 ; 0 0 ]
-    # TODO: test
-    GroupTools.compose_matrix_op(G, G, identity_element(G), op, mat, DefaultOrthonormalBasis(), DefaultOrthonormalBasis())
-end
 
 compose_matrix_op_(G,M,p,op,mat,BG,BM) = begin
     vecs = [op(get_vector_lie(G, b, BG)) for b in eachcol(mat)]
