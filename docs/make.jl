18e399dec32cacebb9d15c806aec72db36a0805f
Olivier Verdier <olivier.verdier@gmail.com>
2024-07-03 20:36:56 +0200

    Add deploydoc

--- docs/make.jl
+++ docs/make.jl
@@ -22,6 +22,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo="github.com/olivierverdier/GroupTools.jl.git",
+)
